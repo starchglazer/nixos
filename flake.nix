e79a0cf6294cdfadd9d6234f7ab3ec3d257c6a8e
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-13 04:07:05 +0800

    flake: initial dandelion setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "nixos go brrrr";
+
+  # yoinked @Rexcrazy804's cute nixos dandelion setup
+  # https://github.com/Rexcrazy804/Zaphkiel
+  outputs = { ... } @ inputs:
+  let
+    dandelion = import ./dandelion.nix inputs;
+    inherit (dandelion) importModules recursiveImport;
+  in importModules [
+    (recursiveImport ./modules)
+    (recursiveImport ./flake)
+  ];
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    hjem = {
+      url = "github:feel-co/hjem";
+      inputs.smfh.follows = "";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nix-darwin.follows = "";
+    };
+    hjem-impure = {
+      url = "github:Rexcrazy804/hjem-impure";
+      inputs.nixpkgs.follows = "";
+      inputs.hjem.follows = "";
+    };
+  };
+}

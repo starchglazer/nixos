6b2c6b749186878d623a6308601fe37bf5d7cce5
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-13 10:30:34 +0800

    profiles: add workstation profile

---
+++ modules/profiles/workstation.nix
@@ -0,0 +1,19 @@
+{ self, ... }: {
+  dandelion.profiles.workstation = {
+    imports = [
+      self.dandelion.modules.audio
+      self.dandelion.modules.boot
+      self.dandelion.modules.bluetooth
+      self.dandelion.modules.firmwares
+      self.dandelion.modules.fonts
+      self.dandelion.modules.graphics
+      self.dandelion.modules.network
+
+      self.dandelion.modules.hjem
+      self.dandelion.modules.hjem-impure
+
+      # self.dandelion.modules.browsers
+      # self.dandelion.modules.office
+    ];
+  };
+}

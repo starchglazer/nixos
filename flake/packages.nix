ffa9e0f494a8015345c508e13661a4bd66da93c8
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-13 10:19:42 +0800

    flake: add packages

---
+++ flake/packages.nix
@@ -0,0 +1,13 @@
+{ self, nixpkgs, ... }:
+let
+  inherit (nixpkgs.lib) filesystem callPackageWith;
+in {
+  packages = self.lib.eachSystem (
+    { pkgs, pkgx, ... }:
+    filesystem.packagesFromDirectoryRecursive {
+      inherit (pkgs) newScope;
+      callPackage = callPackageWith (pkgs // pkgx);
+      directory = self.paths.pkgs;
+    }
+  );
+}

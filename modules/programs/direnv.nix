9550fbf01d30912348bc9fe47fe99461ea09beca
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-13 10:26:57 +0800

    programs: add direnv module

---
+++ modules/programs/direnv.nix
@@ -0,0 +1,22 @@
+{
+  dandelion.modules.direnv = {
+    programs.direnv = {
+      enable = true;
+      silent = true;
+      loadInNixShell = true;
+      nix-direnv.enable = true;
+      direnvrcExtra = ''
+        : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
+        declare -A direnv_layout_dirs
+        direnv_layout_dir() {
+            local hash path
+            echo "''${direnv_layout_dirs[$PWD]:=$(
+                hash="$(sha1sum - <<< "$PWD" | head -c40)"
+                path="''${PWD//[^a-zA-Z0-9]/-}"
+                echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+            )}"
+        }
+      '';
+    };
+  };
+}

a4b237303c766c5454230b44fc88fd7e7e8dac5a
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-13 11:45:54 +0800

    host: add inspiron host module

---
+++ modules/host/inspiron.nix
@@ -0,0 +1,43 @@
+{ self, ... }: {
+  dandelion.host.inspiron = { lib, pkgs, config, ... }:
+  let
+    pkgx = self.lib.mkPkgx' pkgs;
+  in {
+    imports = [
+      # hardware
+      self.dandelion.modules.intel
+
+      # a common user's commonly shared profiles/dots
+      self.dandelion.users.common
+      self.dandelion.profiles.common
+      # self.dandelion.dots.common          # <-- TODO: move common dots here soon
+
+      # host-specific profiles
+      self.dandelion.profiles.gaming
+      # self.dandelion.profiles.gnome
+
+      # host-specific dots
+      # self.dandelion.dots.<dot-name>
+    ];
+
+    networking.hostName = "inspiron";
+    nixpkgs.hostPlatform = "x86_64-linux";
+    system.stateVersion = "24.05";
+
+    fileSystems = {
+      "/" = {
+        device = "/dev/disk/by-uuid/8161a374-4f4e-4c8a-a0df-80b2953295b7";
+        fsType = "ext4";
+      };
+      "/boot" = {
+        device = "/dev/disk/by-uuid/802E-D891";
+        fsType = "vfat";
+        options = [ "fmask=0077" "dmask=0077" ];
+      };
+    };
+
+    swapDevices = [
+      { device = "/dev/disk/by-uuid/ee6f6f68-adef-4fdb-a96c-8c9dd5f7bae2"; }
+    ];
+  };
+}

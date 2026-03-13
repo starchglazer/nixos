b26a6c49b466a56e2099d8ad6e46a3ccb8403f76
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-13 10:20:03 +0800

    flake: add dandelion nixosConfigurations

---
+++ flake/nixosConfigurations.nix
@@ -0,0 +1,9 @@
+{ nixpkgs, self, ... }:
+let
+  inherit (nixpkgs.lib) genAttrs nixosSystem attrNames;
+
+  mkHost = hostName: nixosSystem { modules = [ self.dandelion.hosts.${hostName} ]; };
+  hosts = attrNames self.dandelion.hosts;
+in {
+  nixosConfigurations = genAttrs hosts mkHost;
+}

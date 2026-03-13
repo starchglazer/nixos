15edee443b9858b92cc2776be6bbdef044cc0f84
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-13 10:25:25 +0800

    hardware: add configurable intel module

---
+++ modules/hardware/intel.nix
@@ -0,0 +1,53 @@
+{
+  dandelion.modules.intel = { pkgs, lib, config, ... }:
+  let
+    inherit (lib) mkIf getExe mkOption;
+    inherit (lib.types) enum listOf package;
+
+    cfg = config.opt-ins.graphics.intel;
+  in {
+    options.opt-ins.graphics.intel = {
+      hwAccelDriver = mkOption {
+        type = enum [ "media-driver" "vaapi-driver" ];
+        default = "media-driver";
+        description = "Hardware acceleration driver to use";
+      };
+      driverPackages = mkOption {
+        readOnly = true;
+        default =
+          if cfg.hwAccelDriver == "media-driver"
+          then [
+            pkgs.intel-media-driver
+            pkgs.libvdpau-va-gl
+            pkgs.vpl-gpu-rt
+          ]
+          else [ pkgs.intel-vaapi-driver ];
+        description = "readonly list of packages for selected driver";
+      };
+      runtimePackages = mkOption {
+        type = listOf package;
+        default = [
+          pkgs.intel-compute-runtime
+          pkgs.intel-ocl
+        ];
+        description = "list of runtime packages";
+      };
+    };
+
+    config = {
+      hardware.graphics.extraPackages = cfg.runtimePackages ++ cfg.driverPackages;
+
+      # enables frame buffer compression
+      # boot.kernelParams = [ "i915.enable_fbc=1" ]; # unsure what this does
+
+      environment.sessionVariables = {
+        # not supported yet
+        # ANV_VIDEO_DECODE = 1;
+        LIBVA_DRIVER_NAME = mkIf (cfg.hwAccelDriver == "media-driver") "iHD";
+      };
+
+      boot.kernelModules = [ "kvm-intel" ];
+      hardware.cpu.intel.updateMicrocode = true;
+    };
+  };
+}

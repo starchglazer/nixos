48acf875a26becfafb5681d04301c13958b8d3d1
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-16 14:53:34 +0800

    fonts: improve comments and update font package list for compatibility

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -4,20 +4,20 @@
       enableDefaultPackages = true;
       fontDir.enable = true;
 
-      # Idrk why I have lots of fonts, but I do.
-      # Prolly because libreoffice/openoffice being
-      # fucked without some of them, but whatever.
+      # There are unmentioned packages I previously had/still have
+      # that had font issues or were completely fucked without some of these.
+      #
+      # Apparently, only-office is also completely fucked without some of these.
+      # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/on/onlyoffice-desktopeditors/package.nix#L61-L66
       packages = with pkgs; [
-        freefont_ttf liberation_ttf gyre-fonts roboto unifont
-
-        comic-neue corefonts dejavu_fonts inter lato lexend noto-fonts
-        noto-fonts-cjk-sans work-sans
-
+        # These were added to fix aforementioned font issues as well as
+        # to add core fonts / emoji fonts for better compatibility.
+        freefont_ttf liberation_ttf vista-fonts corefonts unifont noto-fonts noto-fonts-cjk-sans
         noto-fonts-color-emoji openmoji-color openmoji-black twemoji-color-font
 
-        nerd-fonts.dejavu-sans-mono nerd-fonts.droid-sans-mono
-        nerd-fonts.jetbrains-mono
-
+        # Only added for ricing to match design systems, personal use, or just because I like them.
+        comic-neue dejavu_fonts dm-sans gyre-fonts inter lato lexend roboto work-sans
+        nerd-fonts.dejavu-sans-mono nerd-fonts.droid-sans-mono nerd-fonts.jetbrains-mono
         nerd-fonts.symbols-only material-symbols
       ];
     };

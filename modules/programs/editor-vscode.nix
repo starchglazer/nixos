c5ab2c28f3701d6f288460d5ba3b62fc30e73116
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-19 10:37:04 +0800

    programs: add vscode editor + extensions module

---
+++ modules/programs/editor-vscode.nix
@@ -0,0 +1,42 @@
+{
+  dandelion.modules.editor-vscode = {
+    environment.systemPackages = with pkgs; [
+      (vscode-with-extensions.override {
+        vscodeExtensions = with vscodeExtensions; [
+          adpyke.codesnap
+          bradlc.vscode-tailwindcss
+          christian-kohler.npm-intellisense
+          dbaeumer.vscode-eslint
+          esbenp.prettier-vscode
+          expo.vscode-expo-tools
+          formulahendry.code-runner
+          github.codespaces
+          github.copilot
+          github.copilot-chat
+          github.github-vscode-theme
+          github.vscode-pull-request-github
+          jnoortheen.nix-ide
+          jock.svg
+          mhutchie.git-graph
+          mkhl.direnv
+          ms-vscode.cmake-tools
+          ms-vscode.vscode-websearchforcopilot
+          pkief.material-icon-theme
+          redhat.vscode-yaml
+          rvest.vs-code-prettier-eslint
+          sndst00m.vscode-native-svg-preview
+          theqtcompany.qt
+          theqtcompany.qt-core
+          theqtcompany.qt-cpp
+          theqtcompany.qt-qml
+          theqtcompany.qt-ui
+          tomoki1207.pdf
+          yoavbls.pretty-ts-errors
+          yzhang.markdown-all-in-one
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+
+        ];
+      })
+    ];
+  };
+}

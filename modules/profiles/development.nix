5c2156392ca0d500b90e55e1e8b982b3d01176f7
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-19 17:44:35 +0800

    profiles: reorganize imports in development profile

--- modules/profiles/development.nix
+++ modules/profiles/development.nix
@@ -1,9 +1,14 @@
 { self, ... }: {
   dandelion.profiles.development = {
     imports = [
+      # modules
       self.dandelion.modules.direnv
       self.dandelion.modules.env
       self.dandelion.modules.editors
+
+      # dots
+      self.dandelion.dots.env
+      self.dandelion.dots.editors
     ];
   };
 }

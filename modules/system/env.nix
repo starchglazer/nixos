5ec9cd944c83c4ffef48b7fa79b8171e2c06f1f2
starchglazer <190091550+starchglazer@users.noreply.github.com>
2026-03-19 17:45:40 +0800

    programs: improve environment configuration

--- modules/system/env.nix
+++ modules/system/env.nix
@@ -1,4 +1,8 @@
-{
+{ config, self, ... }:
+let
+  inherit (self.lib) mkDotsModule;
+  username = config.networking.hostName;
+in {
   dandelion.modules.env = { pkgs, ... }: {
     environment = {
       systemPackages = with pkgs; [
@@ -9,5 +13,9 @@
       variables.EDITOR = "vim";
       sessionVariables.NIXOS_OZONE_WL = "1";
     };
+  };
+
+  dandelion.dots.env = mkDotsModule username {
+
   };
 }
